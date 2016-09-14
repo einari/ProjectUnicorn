e3cdf077aa88c51f52177adf6e782fe4ea0d9061
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-14 22:25:37 +0200

    Sku = Category.. :)

--- Source/Web/Catalog/CatalogController.cs
+++ Source/Web/Catalog/CatalogController.cs
@@ -10,8 +10,8 @@ namespace Web.Catalog
         [HttpGet("{category}")]
         public IEnumerable<Product> Get(string category)
         {
-            if( sku == "slow") Thread.Sleep(1000);
-            if( sku == "superslow") Thread.Sleep(10000);
+            if( category == "slow") Thread.Sleep(1000);
+            if( category == "superslow") Thread.Sleep(10000);
 
             return new Product[] {
                 new Product { Sku = "123456", Title = "Something cool", Price = 100 }
