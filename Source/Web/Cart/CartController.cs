3f337c846b990ece88b018012d6e995631b1ea88
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-14 22:30:21 +0200

    Making the post a put - probably more correct

--- Source/Web/Cart/CartController.cs
+++ Source/Web/Cart/CartController.cs
@@ -6,8 +6,8 @@ namespace Web.Cart
     [Route("api/[controller]")]
     public class CartController : Controller
     {
-        [HttpPost]
-        public void Post([FromBody] string sku, [FromBody] int quantity)
+        [HttpPut]
+        public void Put([FromBody] string sku, [FromBody] int quantity)
         {
             if( sku == "slow") Thread.Sleep(1000);
             if( sku == "superslow") Thread.Sleep(10000);
