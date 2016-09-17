719d714c17b5838c2785ec530e4627e4e2793610
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-18 00:51:10 +0200

    Introducing JSPM for package management

---
+++ Source/Web/jspm.config.js
@@ -0,0 +1,48 @@
+SystemJS.config({
+  browserConfig: {
+    "paths": {
+      "npm:": "/jspm_packages/npm/",
+      "github:": "/jspm_packages/github/",
+      "web/": "/src/"
+    }
+  },
+  nodeConfig: {
+    "paths": {
+      "npm:": "jspm_packages/npm/",
+      "github:": "jspm_packages/github/",
+      "web/": "src/"
+    }
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.15",
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "core-js": "npm:core-js@1.2.7",
+      "babel": "npm:babel-core@5.8.38",
+      "path": "github:jspm/nodelibs-path@0.2.0-alpha",
+      "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+      "fs": "github:jspm/nodelibs-fs@0.2.0-alpha"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "web": {
+      "main": "web.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  map: {},
+  packages: {}
+});
