326ed0ad8f7e6212fa1835fe1936cce283ed7ae9
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-22 12:28:00 +0200

    Core setup with JSPM

--- Source/Web/index.js
+++ Source/Web/index.js
@@ -1,1 +1,12 @@
-alert("Hello");
+import * as ko from "knockout";
+
+export class index
+{
+    constructor() {
+        var self = this;
+
+        this.timer = ko.observable(0);
+
+        setInterval(() => self.timer(self.timer()+1),1000);
+    }
+} 
