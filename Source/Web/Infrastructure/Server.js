be00e8a9820a4d3202b3527e911757f0b1cd1d6c
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-23 17:13:47 +0200

    Adding support for PUT

--- Source/Web/Infrastructure/Server.js
+++ Source/Web/Infrastructure/Server.js
@@ -21,5 +21,25 @@ class Server
 
         return promise;
     }
+
+    put(url, parameters) {
+        let promise = new Promise((resolve, reject) => {
+            $.ajax({
+                url: url,
+                type: "PUT",
+                dataType: "json",
+                data: parameters,
+                contentType: "application/json; charset=utf-8",
+                complete: (result, statusAsText) => {
+                    resolve(result);
+                },
+                error: (jqXHR, statusAsText, errorThrown) => {
+                    reject(jqXHR);
+                }
+            });
+        });
+
+        return promise;
+    }
 }
 export let server = new Server();
