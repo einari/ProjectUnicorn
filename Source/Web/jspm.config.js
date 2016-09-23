9611ecafafb9eadcac68a1d5c7744c4484a7cac7
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-23 17:12:45 +0200

    Configuring packages related to jQuery + SignalR

--- Source/Web/jspm.config.js
+++ Source/Web/jspm.config.js
@@ -1,59 +1,80 @@
 SystemJS.config({
-  defaultJSExtensions: true,    
-  browserConfig: {
-    "paths": {
-      "npm:": "/jspm_packages/npm/",
-      "github:": "/jspm_packages/github/",
-      "web/": "/src/"
-    }
-  },
-  nodeConfig: {
-    "paths": {
-      "npm:": "jspm_packages/npm/",
-      "github:": "jspm_packages/github/",
-      "web/": "src/"
-    }
-  },
-  devConfig: {
+    defaultJSExtensions: true,
     "map": {
-      "plugin-babel": "npm:systemjs-plugin-babel@0.0.15",
-      "babel-runtime": "npm:babel-runtime@5.8.38",
-      "core-js": "npm:core-js@1.2.7",
-      "babel": "npm:babel-core@5.8.38",
-      "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-      "process": "github:jspm/nodelibs-process@0.2.0-alpha",
-      "fs": "github:jspm/nodelibs-fs@0.2.0-alpha"
-    }
-  },
-  transpiler: "plugin-babel",
-  packages: {
-    "web": {
-      "main": "web.js",
-      "meta": {
-        "*.js": {
-          "loader": "plugin-babel"
+        "jquery": "Scripts/jquery-2.2.4.min",
+        "signalr-jquery": "Scripts/jquery.signalr-2.2.0",
+        "signalr": "signalr"
+    },
+    "meta": {
+        "signalr-jquery": {
+            "format": "global",
+            "deps": ["jquery"]
         }
-      }
+    },
+
+    browserConfig: {
+        "paths": {
+            "npm:": "/jspm_packages/npm/",
+            "github:": "/jspm_packages/github/",
+            "web/": "/src/"
+        }
+    },
+    nodeConfig: {
+        "paths": {
+            "npm:": "jspm_packages/npm/",
+            "github:": "jspm_packages/github/",
+            "web/": "src/"
+        }
+    },
+    devConfig: {
+        "map": {
+            "plugin-babel": "npm:systemjs-plugin-babel@0.0.15",
+            "babel-runtime": "npm:babel-runtime@5.8.38",
+            "core-js": "npm:core-js@1.2.7",
+            "babel": "npm:babel-core@5.8.38",
+            "path": "github:jspm/nodelibs-path@0.2.0-alpha",
+            "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+            "fs": "github:jspm/nodelibs-fs@0.2.0-alpha"
+        }
+    },
+    transpiler: "plugin-babel",
+    packages: {
+        "web": {
+            "main": "web.js",
+            "meta": {
+                "*.js": {
+                    "loader": "plugin-babel"
+                }
+            }
+        },
+        "signalr": {
+            "format": "global",
+            "defaultExtension": false,
+            "meta": {
+                "js": {
+                    "format": "global",
+                    "deps": ["signalr-jquery"]
+                }
+            }
+        }
     }
-  }
 });
 
 SystemJS.config({
-  packageConfigPaths: [
-    "npm:@*/*.json",
-    "npm:*.json",
-    "github:*/*.json"
-  ],
-  map: {
-    "html": "github:Hypercubed/systemjs-plugin-html@0.0.8",
-    "jquery": "npm:jquery@3.1.1",
-    "knockout": "github:knockout/knockout@3.4.0"
-  },
-  packages: {
-    "github:Hypercubed/systemjs-plugin-html@0.0.8": {
-      "map": {
-        "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.7.22"
-      }
+    packageConfigPaths: [
+        "npm:@*/*.json",
+        "npm:*.json",
+        "github:*/*.json"
+    ],
+    map: {
+        "html": "github:Hypercubed/systemjs-plugin-html@0.0.8",
+        "knockout": "github:knockout/knockout@3.4.0"
+    },
+    packages: {
+        "github:Hypercubed/systemjs-plugin-html@0.0.8": {
+            "map": {
+                "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.7.22"
+            }
+        }
     }
-  }
 });
