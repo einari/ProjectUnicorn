caa33bec9dc59d0bfdb117c643d9b24bb5319817
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-23 11:29:04 +0200

    Simple hookup for Catalog

--- Source/Web/Catalog/index.js
+++ Source/Web/Catalog/index.js
@@ -1,7 +1,14 @@
+import {server} from "../Infrastructure/Server"
+
 export class index
 {
     constructor() {
-        this.something = "Something";
-    }
+        let self = this;
 
+        this.products = ko.observableArray();
+        
+        server.get("/api/catalog/something",{}).then(result => {
+            self.products(result);
+        });
+    }
 }
