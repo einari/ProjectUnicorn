61f67aee0dcf67e557347b1f5d008ffc19e41902
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-25 00:07:47 +0200

    Adding a catalog with some content

--- Source/Web/Catalog/CatalogController.cs
+++ Source/Web/Catalog/CatalogController.cs
@@ -14,7 +14,13 @@ namespace Web.Catalog
             if( category == "superslow") Thread.Sleep(10000);
 
             return new Product[] {
-                new Product { Sku = "123456", Title = "Something cool", Price = 100 }
+                new Product { Sku = "35906", Title = "Venstre bak", Description = "For bilder med bremseskiver som er 278 mm i diameter. For biler med 12mm tykke bremseskiver", Price = 2709 },
+                new Product { Sku = "35907", Title = "Høyre bak", Description = "For bilder med bremseskiver som er 278 mm i diameter. For biler med 12mm tykke bremseskiver", Price = 2709 },
+                new Product { Sku = "72507", Title = "Venstre bak", Description = "Uten glider", Price = 1898 },
+                new Product { Sku = "72508", Title = "Høyre bak", Description = "Uten glider", Price = 2709 },
+                new Product { Sku = "27163", Title = "Xtreme", Description = "Xtreme-emblem. Dobbelsidig tape på baksiden. 115x40mm", Price = 59 },
+                new Product { Sku = "27159", Title = "V6", Description = "V6-emblem. Dobbelsidig tape på baksiden. 70x35", Price = 39 },
+                new Product { Sku = "27418", Title = "Tuing", Description = "Tunign-emblem. Dobbelsidig tape på baksiden. 105x23mm", Price = 39 }
             };
         }
     }
