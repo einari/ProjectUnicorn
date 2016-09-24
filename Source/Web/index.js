0c682257e405400ce09cd9ae4bb7fe2912ca5959
Einar Ingebrigtsen <einar@dolittle.com>
2016-09-25 00:08:06 +0200

    Removing timer test + moving cart out

--- Source/Web/index.js
+++ Source/Web/index.js
@@ -3,10 +3,5 @@ import * as ko from "knockout";
 export class index
 {
     constructor() {
-        var self = this;
-
-        this.timer = ko.observable(0);
-
-        setInterval(() => self.timer(self.timer()+1),1000);
     }
 } 
